ea5aae1b7b2f65da2ba229765de816012490f279
R. Vaccari <rodrigo.melo@penguinformula.com>
2024-11-06 11:42:22 +0000

    dbhelper updates

--- app/src/main/java/com/ipcb/milleuro/DBHelper.java
+++ app/src/main/java/com/ipcb/milleuro/DBHelper.java
@@ -1,11 +1,19 @@
 package com.ipcb.milleuro;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import com.ipcb.milleuro.model.Answer;
+import com.ipcb.milleuro.model.Difficulty;
+import com.ipcb.milleuro.model.Question;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DBHelper extends SQLiteOpenHelper {
 
@@ -31,9 +39,10 @@ public class DBHelper extends SQLiteOpenHelper {
 
         db.execSQL("CREATE TABLE " + QUESTION_TABLE + "("
                 + "id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + "difficultyId INTEGER NOT NULL, "
+                + "questionText TEXT NOT NULL, "
+                + "value INTEGER NOT NULL, "
                 + "correctAnswerId INTEGER NOT NULL, "
-                + "name TEXT NOT NULL, "
+                + "difficultyId INTEGER NOT NULL, "
                 + "FOREIGN KEY (correctAnswerId) REFERENCES Answer(id),"
                 + "FOREIGN KEY (difficultyId) REFERENCES Difficulty(id))");
 
@@ -52,4 +61,60 @@ public class DBHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
+    public List<Difficulty> getDifficulties() {
+        List<Difficulty> results = new ArrayList<>();
+        try (Cursor cursor = getAllFromTable(DIFF_TABLE)) {
+            results.add(new Difficulty(cursor.getInt(0),
+                    cursor.getInt(1),
+                    cursor.getString(2)));
+        }
+
+        return results;
+    }
+
+    public List<Answer> getAnswers() {
+        List<Answer> results = new ArrayList<>();
+        try (Cursor cursor = getAllFromTable(ANSWER_TABLE)) {
+            results.add(new Answer(cursor.getInt(0),
+                    cursor.getString(1)));
+        }
+
+        return results;
+    }
+
+    public List<Question> getQuestions() {
+        List<Question> results = new ArrayList<>();
+        try (Cursor cursor = getAllFromTable(ANSWER_TABLE)) {
+            results.add(new Question(cursor.getInt(0),
+                    cursor.getString(1),
+                    null,
+                    null,
+                    null,
+                    cursor.getInt(2)));
+        }
+
+        return results;
+    }
+
+    private Cursor getAllFromTable(String table) {
+        final SQLiteDatabase db = this.getReadableDatabase();
+        return db.query(table,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null);
+    }
+
+    private Cursor getDifficultyById(int id) {
+        final SQLiteDatabase db = this.getReadableDatabase();
+        return db.query(DIFF_TABLE,
+                null,
+                "id = " + id,
+                null,
+                null,
+                null,
+                null);
+    }
 }
