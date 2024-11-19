6131f1e496401bad49d511fd9fefca9e4ed66ab9
Kenny-Boy <lucaslukeny6@gmai.com>
2024-11-19 12:07:41 +0000

    GameActivity added

--- app/src/main/java/com/ipcb/milleuro/GameActivity.java
+++ app/src/main/java/com/ipcb/milleuro/GameActivity.java
@@ -1,10 +1,8 @@
 package com.ipcb.milleuro;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextPaint;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,33 +10,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity {
 
-    Button btnStart;
-    EditText txtName;
+    TextView txtName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_game);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        btnStart.findViewById(R.id.Main_btnStart);
-        txtName.findViewById(R.id.Main_txtName);
-
-        btnStart.setOnClickListener(view -> {
-            //String mensagem = "Este texto é para abrir com um bloco notas!";
-
-            Intent openGame = new Intent(this, GameActivity.class);
-            openGame.setAction(Intent.ACTION_SEND);
-            openGame.putExtra("PlayerName", txtName.getText());
-
-            startActivity(openGame);
-        });
+        Bundle extras = getIntent().getExtras();
+        String playerName = extras.getString("PlayerName");
+        txtName.findViewById(R.id.Game_txtName);
+        txtName.setText(playerName);
     }
 }
