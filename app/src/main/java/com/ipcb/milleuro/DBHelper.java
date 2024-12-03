e41a2f3a34ae53d2b01258df90e65e05999d5552
Kenny-Boy <lucaslukeny6@gmai.com>
2024-12-03 12:14:47 +0000

    Insert INTOS Tables diff and answers

--- app/src/main/java/com/ipcb/milleuro/DBHelper.java
+++ app/src/main/java/com/ipcb/milleuro/DBHelper.java
@@ -28,6 +28,10 @@ public class DBHelper extends SQLiteOpenHelper {
                                 QUESTION_TABLE = "Question",
                                 QUESTION_ANSWER_TABLE = "QuestionAnswer";
 
+    String easy = ", Fácil)";
+    String medium = ", Médio)";
+    String hard = ", Dificil)";
+
     public DBHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
@@ -58,6 +62,52 @@ public class DBHelper extends SQLiteOpenHelper {
                 + "FOREIGN KEY (questionId) REFERENCES Question(id),"
                 + "FOREIGN KEY (answerId) REFERENCES Answer(id),"
                 + "PRIMARY KEY (questionId, answerId))");
+
+        db.execSQL("INSERT INTO " + DIFF_TABLE + " VALUES ("
+                + 1
+                + easy );
+
+        db.execSQL("INSERT INTO " + DIFF_TABLE + " VALUES ("
+                + 2
+                + medium );
+
+        db.execSQL("INSERT INTO " + DIFF_TABLE + " VALUES ("
+                + 3
+                + hard );
+
+        db.execSQL("INSERT INTO " + ANSWER_TABLE + " VALUES "
+                + "( 'Paris' ),"
+                + "( 'Leonardo da Vinci' ),"
+                + "( 'Português' ),"
+                + "( '60' ),"
+                + "( 'George Washington' ),"
+                + "( 'Mercúrio' ),"
+                + "( 'Oceano Pacífico' ),"
+                + "( '60' ),"
+                + "( 'Yen' ),"
+                + "( 'África' ),"
+                + "( 'Vaticano' ),"
+                + "( 'H₂O' ),"
+                + "( 'Pedro Álvares Cabral' ),"
+                + "( 'William Shakespeare' ),"
+                + "( 'Plutão' ),"
+                + "( 'Pele' ),"
+                + "( 'Célula' ),"
+                + "( '1969' ),"
+                + "( 'Siddhartha Gautama' ),"
+                + "( 'Canadá' ),"
+                + "( 'Miguel de Cervantes' ),"
+                + "( 'Au' ),"
+                + "( 'Baleia Azul' ),"
+                + "( 'J.K. Rowling' ),"
+                + "( 'União Soviética' ),"
+                + "( 'Via Láctea' ),"
+                + "( '1789' ),"
+                + "( 'Suécia' ),"
+                + "( 'Nitrogênio' ),"
+                + "( 'Chita' );");
+
+
     }
 
     @Override
@@ -104,40 +154,41 @@ public class DBHelper extends SQLiteOpenHelper {
     public List<Question> getQuestions() throws NullPointerException {
         List<Question> results = new ArrayList<>();
         try (Cursor cursor = getAllFromTable(QUESTION_TABLE)) {
-            final int difficultyId = cursor.getInt(4);
-            final int correctAnswerId = cursor.getInt(3);
-            final int questionId = cursor.getInt(0);
+            if (cursor.moveToFirst()) {
+                final int difficultyId = cursor.getInt(4);
+                final int correctAnswerId = cursor.getInt(3);
+                final int questionId = cursor.getInt(0);
 
-            final Set<Answer> answers = getAvailableAnswers(questionId);
-            final Answer correctAnswer = getCorrectAnswerById(correctAnswerId);
-            final Difficulty difficulty = getDifficultyById(difficultyId);
+                final List<Answer> answers = getAvailableAnswers(questionId);
+                final Answer correctAnswer = getCorrectAnswerById(correctAnswerId);
+                final Difficulty difficulty = getDifficultyById(difficultyId);
 
-            if (answers.size() != 4)
-                throw new NullPointerException(String.format(
-                        "Question with id [%s] does not have 4 answers!",
-                        questionId
-                ));
+                if (answers.size() != 4)
+                    throw new NullPointerException(String.format(
+                            "Question with id [%s] does not have 4 answers!",
+                            questionId
+                    ));
 
-            if (correctAnswer == null)
-                throw new NullPointerException(String.format(
-                        "Correct answer not found for id [%s] for question with id [%s]",
-                        correctAnswerId,
-                        questionId));
+                if (correctAnswer == null)
+                    throw new NullPointerException(String.format(
+                            "Correct answer not found for id [%s] for question with id [%s]",
+                            correctAnswerId,
+                            questionId));
 
-            if (difficulty == null)
-                throw new NullPointerException(String.format(
-                        "Difficulty not found for id [%s] for question with id [%s]",
-                        difficultyId,
-                        questionId));
+                if (difficulty == null)
+                    throw new NullPointerException(String.format(
+                            "Difficulty not found for id [%s] for question with id [%s]",
+                            difficultyId,
+                            questionId));
 
-            results.add(new Question(questionId,
-                    cursor.getString(1),
-                    answers,
-                    correctAnswer,
-                    difficulty,
-                    cursor.getInt(2)));
+                results.add(new Question(questionId,
+                        cursor.getString(1),
+                        answers,
+                        correctAnswer,
+                        difficulty,
+                        cursor.getInt(2)));
+            }
         }
-
         return results;
     }
 
@@ -183,9 +234,9 @@ public class DBHelper extends SQLiteOpenHelper {
         }
     }
 
-    private Set<Answer> getAvailableAnswers(int questionId) {
+    private List<Answer> getAvailableAnswers(int questionId) {
         final SQLiteDatabase db = this.getReadableDatabase();
-        final Set<Answer> set = new HashSet<>(4);
+        final List<Answer> set = new ArrayList<>(4);
         try (Cursor cursor = db.query(QUESTION_ANSWER_TABLE,
                 null,
                 "questionId = " + questionId,
@@ -212,4 +263,5 @@ public class DBHelper extends SQLiteOpenHelper {
         }
         return set;
     }
+
 }
--- app/src/main/java/com/ipcb/milleuro/GameActivity.java
+++ app/src/main/java/com/ipcb/milleuro/GameActivity.java
@@ -10,9 +10,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.ipcb.milleuro.model.Answer;
+import com.ipcb.milleuro.model.Question;
+
+import java.util.List;
+
 public class GameActivity extends AppCompatActivity {
 
     TextView txtName, txtGrant, txtQuestion;
+    List<Question> questions;
+    int currentIndex;
 
 
     @Override
@@ -20,22 +27,34 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_game);
+        try (DBHelper db = new DBHelper(this)){
+            questions = db.getQuestions();
+
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        Question quests = questions.get(currentIndex);
+
         txtName = findViewById(R.id.Game_txtPlayerName);
         txtName.setText(String.format("Bem vindo %s", getIntent().getStringExtra("PlayerName")));
         txtGrant = findViewById(R.id.Game_txtGrant);
         txtQuestion = findViewById(R.id.Game_txtQuestion);
-        findViewById(R.id.Game_btnAnswer1).setOnClickListener(view -> {});
-        findViewById(R.id.Game_btnAnswer2).setOnClickListener(view -> {});
-        findViewById(R.id.Game_btnAnswer3).setOnClickListener(view -> {});
-        findViewById(R.id.Game_btnAnswer4).setOnClickListener(view -> {});
+        txtQuestion.setText(quests.getQuestionText());
+        findViewById(R.id.Game_btnAnswer1).setOnClickListener(view -> onClickAnswer(quests.getPossibleAnswers().get(0)));
+        findViewById(R.id.Game_btnAnswer2).setOnClickListener(view -> onClickAnswer(quests.getPossibleAnswers().get(1)));
+        findViewById(R.id.Game_btnAnswer3).setOnClickListener(view -> onClickAnswer(quests.getPossibleAnswers().get(2)));
+        findViewById(R.id.Game_btnAnswer4).setOnClickListener(view -> onClickAnswer(quests.getPossibleAnswers().get(3)));
         findViewById(R.id.Game_btnExit).setOnClickListener(view -> {});
 
+
+    }
+
+    public void onClickAnswer(Answer ans){
 
     }
 }
